379d486d5586a6f490fd0d49fa57811d773a10c7
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-08-12 14:17:10 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://greencart-2-fa9i.onrender.com'
+  }));
 app.use(express.json());
 
 app.use('/api', apiRoutes);
